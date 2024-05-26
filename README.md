3a7e687f6540053c40d00efbce4d75af758760c3
vaebe <18137693952@163.com>
2024-05-26 22:37:37 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,23 @@
+# geo-ip2-api
+根据 ip 获取地理信息在线调用版
+
+# 使用 [vercel](https://vercel.com) 部署
+
+```bash
+git clone 
+```
+
+## Install the Vercel CLI:
+
+```bash
+npm i -g vercel
+```
+
+## Then run the app at the root of the repository:
+
+```bash
+vercel dev
+```
+# 使用
+
+
---
+++ api/query.ts
@@ -0,0 +1,20 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { readFileSync } from 'fs'
+import maxmind, { CityResponse, Reader } from 'maxmind';
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // 优先获取 query 参数,如果获取不到则获取本次请求的 req.headers.host
+  const ipStr = req.query?.ip as string ?? req.headers.host
+
+  const buffer = readFileSync('data/GeoLite2-City.mmdb');
+  const lookup = new Reader<CityResponse>(buffer);
+  const ipInfo = lookup.get(ipStr) ?? {}
+
+  console.log(ipStr)
+
+  return res.json(ipInfo)
+}
