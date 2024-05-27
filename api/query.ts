b3c2fb478ed610400e8cc76437e35eee8bc848fa
vaebe <18137693952@163.com>
2024-05-27 09:38:22 +0800

    fix: 未传入 ip 时获取当前请求的 ip 方式

--- api/query.ts
+++ api/query.ts
@@ -8,8 +8,12 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  // 优先获取 query 参数,如果获取不到则获取本次请求的 req.headers.host
-  const ipStr = req.query?.ip as string ?? req.headers.host
+  // 优先获取 query 参数,如果获取不到则获取请求头中的 x-forwarded-for 或 x-real-ip
+  let ipStr = req.query?.ip as string || req.headers['x-forwarded-for'] as string || req.headers['x-real-ip'] as string;
+
+  if (!maxmind.validate(ipStr)) {
+    return res.json({code: 1, msg: 'ip 地址格式不正确!'})
+  }
 
   const filePath = join(process.cwd(), 'data', 'GeoLite2-City.mmdb');
   const buffer = readFileSync(filePath);
