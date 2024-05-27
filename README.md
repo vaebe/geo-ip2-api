c93334a595ad974d0008a2093d1cc6c72123c166
vaebe <18137693952@163.com>
2024-05-27 20:56:09 +0800

    feat: 返回数据格式化

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 # 使用 [vercel](https://vercel.com) 部署
 
 ```bash
-git clone 
+git clone https://github.com/vaebe/geo-ip2-api
 ```
 
 ## Install the Vercel CLI:
@@ -20,4 +20,105 @@ vercel dev
 ```
 # 使用
 
+`code: 0` 表示获取数据正常, 目前仅支持 `https` 调用!
+
+### 获取格式化后的数据 `https://geo-ip2-api.vercel.app/api/query?ip=159.226.171.49&lang=zh-CN`
+```json
+{
+  "code": 0,
+  "msg": "success!",
+  "ip": "159.226.171.49",
+  "continent": "亚洲",
+  "continentCode": "AS",
+  "country": "中国",
+  "countryCode": "CN",
+  "region": "中国",
+  "regionName": "CN",
+  "city": "天津",
+  "lat": 39.1488,
+  "lon": 117.1762,
+  "timezone": "Asia/Shanghai"
+}
+
+```
+
+### 获取未格式化的数据 `https://geo-ip2-api.vercel.app/api/query?ip=139.226.171.49`
+```json
+{
+  "code": 0,
+  "msg": "success!",
+  "city": {
+    "geoname_id": 1792947,
+    "names": {
+      "de": "Tianjin",
+      "en": "Tianjin",
+      "es": "Tianjín",
+      "fr": "Tianjin",
+      "ja": "天津市",
+      "pt-BR": "Tianjin",
+      "ru": "Тяньцзинь",
+      "zh-CN": "天津"
+    }
+  },
+  "continent": {
+    "code": "AS",
+    "geoname_id": 6255147,
+    "names": {
+      "de": "Asien",
+      "en": "Asia",
+      "es": "Asia",
+      "fr": "Asie",
+      "ja": "アジア",
+      "pt-BR": "Ásia",
+      "ru": "Азия",
+      "zh-CN": "亚洲"
+    }
+  },
+  "country": {
+    "geoname_id": 1814991,
+    "iso_code": "CN",
+    "names": {
+      "de": "China",
+      "en": "China",
+      "es": "China",
+      "fr": "Chine",
+      "ja": "中国",
+      "pt-BR": "China",
+      "ru": "Китай",
+      "zh-CN": "中国"
+    }
+  },
+  "location": {
+    "accuracy_radius": 100,
+    "latitude": 39.1488,
+    "longitude": 117.1762,
+    "time_zone": "Asia/Shanghai"
+  },
+  "registered_country": {
+    "geoname_id": 1814991,
+    "iso_code": "CN",
+    "names": {
+      "de": "China",
+      "en": "China",
+      "es": "China",
+      "fr": "Chine",
+      "ja": "中国",
+      "pt-BR": "China",
+      "ru": "Китай",
+      "zh-CN": "中国"
+    }
+  },
+  "subdivisions": [
+    {
+      "geoname_id": 1792943,
+      "iso_code": "TJ",
+      "names": {
+        "en": "Tianjin",
+        "fr": "Municipalité de Tianjin",
+        "zh-CN": "天津市"
+      }
+    }
+  ]
+}
+```
 
--- api/query.ts
+++ api/query.ts
@@ -1,7 +1,33 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { readFileSync } from 'fs'
-import {join} from 'path';
+import { join } from 'path';
 import maxmind, { CityResponse, Reader } from 'maxmind';
+
+function formattedData(info: CityResponse, ip: string, lang = 'zh-CN') {
+  const data = {
+    code: 0,
+    msg: 'success!',
+    ip,
+    "continent": info.continent.names[lang],
+    "continentCode": info.continent.code,
+    "country": info.country.names[lang],
+    "countryCode": info.country.iso_code,
+    "region": info.registered_country.names[lang],
+    "regionName": info.registered_country.iso_code,
+    "city": info.city.names[lang] ?? '',
+    "lat": info.location.latitude,
+    "lon": info.location.longitude,
+    "timezone": info.location.time_zone,
+  }
+
+  // 如果检测到香港，将其归类为中国
+  if (info?.country?.iso_code === 'HK') {
+    data.countryCode = 'CN';
+    data.country = '中国';
+  }
+
+  return data
+}
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -12,15 +38,29 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   let ipStr = req.query?.ip as string || req.headers['x-forwarded-for'] as string || req.headers['x-real-ip'] as string;
 
   if (!maxmind.validate(ipStr)) {
-    return res.json({code: 1, msg: 'ip 地址格式不正确!'})
+    return res.json({ code: 1, msg: 'ip 地址格式不正确!' })
   }
 
   const filePath = join(process.cwd(), 'data', 'GeoLite2-City.mmdb');
   const buffer = readFileSync(filePath);
   const lookup = new Reader<CityResponse>(buffer);
-  const ipInfo = lookup.get(ipStr) ?? {}
+  const ipInfo = lookup.get(ipStr)
 
-  console.log(ipStr)
+  if (!ipInfo) {
+    return res.json({ code: 1, msg: `未获取到 ip(${ipStr}) 信息!` })
+  }
 
-  return res.json(ipInfo)
+  // 参数校验
+  const langList = ["de", "en", "es", "fr", "ja", "pt-BR", "ru", "zh-CN"]
+  const paramsLang = req.query.lang as string ?? ''
+  if (paramsLang && !langList.includes(paramsLang)) {
+    return res.json({ code: 1, msg: `参数 lang 不争取, 仅支持${langList.join('、')}` })
+  }
+
+  // paramsLang 存在返回格式化后的数据
+  if (paramsLang) {
+    return res.json(formattedData(ipInfo, ipStr, paramsLang))
+  }
+
+  return res.json({ code: 0, msg: 'success!', ...ipInfo })
 }
